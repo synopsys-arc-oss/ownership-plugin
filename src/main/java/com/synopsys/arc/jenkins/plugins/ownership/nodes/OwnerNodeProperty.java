d1e97ab5fc204408963acc15d1d693fc9ffc5b44
Oleg Nenashev <nenashev@synopsys.com>
2013-05-16 15:22:20 +0400

    [ownership.nodes] - Added function for getting node from stapler request

--- src/main/java/com/synopsys/arc/jenkins/plugins/ownership/nodes/OwnerNodeProperty.java
+++ src/main/java/com/synopsys/arc/jenkins/plugins/ownership/nodes/OwnerNodeProperty.java
@@ -8,10 +8,12 @@ import com.synopsys.arc.jenkins.plugins.ownership.IOwnershipHelper;
 import com.synopsys.arc.jenkins.plugins.ownership.IOwnershipItem;
 import com.synopsys.arc.jenkins.plugins.ownership.OwnershipDescription;
 import hudson.Extension;
+import hudson.model.Computer;
 import hudson.model.Descriptor;
 import hudson.model.Node;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.NodePropertyDescriptor;
+import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
@@ -28,12 +30,13 @@ public class OwnerNodeProperty extends NodeProperty<Node>
     OwnershipDescription ownership;
     
     @DataBoundConstructor
-    public OwnerNodeProperty( OwnershipDescription ownership) {
+    public OwnerNodeProperty(Node node, OwnershipDescription ownership) {
+         setNode(node);
          this.ownership = ownership;
     }
     
     public OwnershipDescription getOwnership()
-    {       
+    {      
         return ownership;
     }
     
@@ -51,12 +54,41 @@ public class OwnerNodeProperty extends NodeProperty<Node>
     public static class DescriptorImpl extends NodePropertyDescriptor {
         
         @Override
-        public NodeProperty<?> newInstance( StaplerRequest req, JSONObject formData ) throws Descriptor.FormException {
-                
+        public NodeProperty<?> newInstance( StaplerRequest req, JSONObject formData ) throws Descriptor.FormException {               
+            Node owner = getNodePropertyOwner(req);
             OwnershipDescription descr = OwnershipDescription.Parse(formData, "slaveOwnership");
-            return descr.isOwnershipEnabled() ? new OwnerNodeProperty(descr) : null;
+            return descr.isOwnershipEnabled() ? new OwnerNodeProperty(owner, descr) : null;       
         }
-
+        
+        
+        
+        /**
+         * Gets Node, which is being configured by StaplerRequest
+         * @remarks Workaround for
+         * @param req StaplerRequest (ex, from NodePropertyDescriptor::newInstance())
+         * @return Instance of the node, which is being configured
+         * 
+         * @since 0.0.5
+         * @author Oleg Nenashev <nenashev@synopsys.com>
+         */
+        private static Node getNodePropertyOwner(StaplerRequest req)
+        {                
+           // Check format;
+           String[] items = req.getOriginalRequestURI().split("/");
+           if (items.length < 4)
+           { 
+               assert false : "Unexpected format of the request. "
+                   + "Supported format is /computer/${NODE_NAME}/"+STAPLER_CONFIGURE_REQ_NAME;
+               return null;
+           }
+           assert items[items.length-1].equals(STAPLER_CONFIGURE_REQ_NAME) : "Unexpected request name.";
+       
+           // Get node from Jenkins
+           String nodeName = items[items.length-2];        
+           return Jenkins.getInstance().getNode(nodeName); 
+        }
+        private static final String STAPLER_CONFIGURE_REQ_NAME = "configSubmit";
+        
         @Override
         public String getDisplayName() {
                 return "Slave Owner";
@@ -69,7 +101,7 @@ public class OwnerNodeProperty extends NodeProperty<Node>
     }
 
     @Override
-    protected void setNode(Node node) {
+    protected final void setNode(Node node) {
         super.setNode(node); //To change body of generated methods, choose Tools | Templates.
     } 
 }
