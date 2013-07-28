ecc5e5a16c9d06015b4f3c3c254e84a102973e62
Oleg Nenashev <nenashev@synopsys.com>
2013-07-29 01:30:32 +0400

    Improved appearance of the pages. Resolves https://issues.jenkins-ci.org/browse/JENKINS-18742
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

--- src/main/java/com/synopsys/arc/jenkins/plugins/ownership/nodes/OwnerNodeProperty.java
+++ src/main/java/com/synopsys/arc/jenkins/plugins/ownership/nodes/OwnerNodeProperty.java
@@ -130,7 +130,7 @@ public class OwnerNodeProperty extends NodeProperty<Node>
         
         @Override
         public String getDisplayName() {
-                return Messages.NodeOwnership_Config_SectionTitle();
+                return Messages.NodeOwnership_Config_SectionTitle()+". (just a stub, use \"Manage ownership\" page to configure)";
         }
 
         @Override
