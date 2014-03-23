d11beb6f5c860c47c569690f473ea81763bcfb96
Oleg Nenashev <o.v.nenashev@gmail.com>
2014-03-23 20:48:59 +0400

    Added a stub for the item-specific security column.
    Signed-off-by: Oleg Nenashev <o.v.nenashev@gmail.com>

---
+++ src/main/java/com/synopsys/arc/jenkins/plugins/ownership/security/itemspecific/ItemSpecificSecurityColumn.java
@@ -0,0 +1,67 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Oleg Nenashev <nenashev@synopsys.com>, Synopsys Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.synopsys.arc.jenkins.plugins.ownership.security.itemspecific;
+
+import com.synopsys.arc.jenkins.plugins.ownership.jobs.*;
+import com.synopsys.arc.jenkins.plugins.ownership.Messages;
+import org.kohsuke.stapler.DataBoundConstructor;
+import hudson.Extension;
+import hudson.model.Job;
+import hudson.views.ListViewColumnDescriptor;
+import hudson.views.ListViewColumn;
+
+/**
+ * Indicates that the job has configured project-specific security settings.
+ * @author Oleg Nenashev <nenashev@synopsys.com>, Synopsys Inc.
+ * @since 0.5
+ */
+public class ItemSpecificSecurityColumn extends ListViewColumn {
+
+    @DataBoundConstructor
+    public ItemSpecificSecurityColumn() {
+        super();
+    }
+    
+    public String getJobOwner(@SuppressWarnings("rawtypes") Job job) {
+        return JobOwnerHelper.Instance.getOwner(job);
+    }
+    
+    public boolean isOwnerExists(@SuppressWarnings("rawtypes") Job job) {
+        return JobOwnerHelper.Instance.isOwnerExists(job);
+    }
+
+    @Extension
+    public static class DescriptorImpl extends ListViewColumnDescriptor {
+  
+        @Override
+        public boolean shownByDefault() {
+            return false;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return Messages.JobOwnership_Column_Title();
+        }
+    }
+}
